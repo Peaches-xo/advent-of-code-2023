0d0339cafe4909c33963d2086d8381d8e4af6b3e
Kimberley Le Bherz <kimberley_lebherz@yahoo.com>
2023-12-06 14:43:22 +1000

    completed problem 1

--- Day 2/data.js
+++ Day 2/data.js
@@ -101,8 +101,12 @@ Game 100: 1 blue, 13 green, 14 red; 11 green, 11 blue, 7 red; 2 red, 1 blue, 2 g
 
 export let testData = `Game 1: 1 green, 1 blue, 1 red; 1 green, 8 red, 7 blue; 6 blue, 10 red; 4 red, 9 blue, 2 green; 1 green, 3 blue; 4 red, 1 green, 10 blue.
 Game 2: 9 red, 7 green, 3 blue; 15 green, 2 blue, 5 red; 10 red, 3 blue, 13 green.
-Game 3: 3 red, 1 blue, 4 green; 6 red, 3 green, 2 blue; 6 red, 16 blue, 1 green.
-Game 4: 2 blue, 2 green, 19 red; 3 blue, 11 red, 16 green; 18 blue, 13 green, 20 red; 18 red, 12 blue, 16 green; 8 green, 16 blue, 16 red.
-Game 5: 8 green, 1 red, 12 blue; 10 green, 6 red, 13 blue; 1 red, 3 blue, 6 green; 14 blue, 2 red, 7 green.`;
+Game 3: 3 red, 1 blue, 4 green; 6 red, 3 green, 2 blue; 6 red, 16 blue, 1 green.`;
+
+
+//Game 1: 1 green, 1 blue, 1 red; 1 green, 8 red, 7 blue; 6 blue, 10 red; 4 red, 9 blue, 2 green; 1 green, 3 blue; 4 red, 1 green, 10 blue.
+// Game 3: 3 red, 1 blue, 4 green; 6 red, 3 green, 2 blue; 6 red, 16 blue, 1 green.
+// Game 4: 2 blue, 2 green, 19 red; 3 blue, 11 red, 16 green; 18 blue, 13 green, 20 red; 18 red, 12 blue, 16 green; 8 green, 16 blue, 16 red.
+// Game 5: 8 green, 1 red, 12 blue; 10 green, 6 red, 13 blue; 1 red, 3 blue, 6 green; 14 blue, 2 red, 7 green.
 
 
--- Day 2/index.js
+++ Day 2/index.js
@@ -13,47 +13,118 @@
 //only 12 red, 13 green, and 14 blue
 
 
-//game object to keep track of which numbers are possible
+import { data } from './data.js';
 
-import { testData } from './data.js';
-
-
-let para = document.querySelector('p');
-let gameArray = testData.split(/\r\n|\n/); //array of strings 
+const para = document.querySelector('p');
+const arrOfPossibleGames  = [];
 
 
+//split the testData string into an array of strings, with each string containing a Game. 
+let arrayOfAllGames = data.split(/\r\n|\n/); 
 
+    //loop through the array of game strings 
+    for (let i = 0; i < arrayOfAllGames.length; i++){
 
-    for (let i = 0; i < gameArray.length; i++){
-
-        let currentGame = gameArray[i];
-            console.log('currentGame: ', currentGame);
+        //get current game string
+        let currentGame = arrayOfAllGames[i];
             //currentGame:  Game 3: 3 red, 1 blue, 4 green; 6 red, 3 green, 2 blue; 6 red, 16 blue, 1 green.
 
-        //get title out 
-        let gameTitle = currentGame.slice(0,(currentGame.indexOf(':')+1));
-            console.log('gameTitle: ', gameTitle);
-            //gameTitle:  Game 3
-
-        //split into subgames
+        //get title  
+        let gameTitle = currentGame.slice(0,(currentGame.indexOf(':')+1)); // Game 3:
+        console.log('gameTitle: ', gameTitle);
+       
+        //get game ID
+        let gameID = Number(currentGame.slice(4,(currentGame.indexOf(':')))); // 3
+   
+        //split game string into array of strings of subgames/hand
         let subGameArray = currentGame.split(';');
-            console.log('subGameArray: ', subGameArray); //array of strings 
-            //['Game 3: 3 red, 1 blue, 4 green', ' 6 red, 3 green, 2 blue', ' 6 red, 16 blue, 1 green.']
 
-        let index = currentGame.indexOf(':');
-            //remove title from first string
-            subGameArray[0] = subGameArray[0].replace(gameTitle,"");
-            console.log('subGameArray after replace: ', subGameArray);
-            //[' 3 red, 1 blue, 4 green', ' 6 red, 3 green, 2 blue', ' 6 red, 16 blue, 1 green.']
+        //remove Game title from first hand
+        subGameArray[0] = subGameArray[0].replace(gameTitle,"");
 
         //remove padding from start of strings
-subGameArray.forEach(string => string.trimStart());  
-console.log('subGameArray after stringTrim: ', subGameArray);
- }
+        subGameArray = subGameArray.map((string) => string.trimStart());
+        console.log('subGameArray: ', subGameArray); //array of subgame strings 
+        //['3 red, 1 blue, 4 green', '6 red, 3 green, 2 blue', '6 red, 16 blue, 1 green.']
+   
+
+        let subGameCounter = 0; 
+        for (let j = 0; j < subGameArray.length; j++){
+            let currentSubGame = subGameArray[j];
+            currentSubGame = currentSubGame.split(',').map((string) => string.trimStart());
+         
+            console.log('currentSubGame: ', currentSubGame); //array of strings with each string being one num/col
+              //   currentSubGame:  (3) ['1 green', '1 blue', '1 red']
+
+            let handCounter= 0;
+            for (let k = 0; k < currentSubGame.length; k++){
+                  console.log('currentSubGame[k]:', currentSubGame[k]);
+              
+                //   currentSubGame[k]: 1 green
+                //   currentSubGame[k]: 1 blue
+                //   currentSubGame[k]: 1 red
+
+                //check if number is one or two digit
+                let firstNum = currentSubGame[k].charAt(0);
+                let secondNum = Number(currentSubGame[k].charAt(1));
+
+                //if number is one digit, (if second char is a space, or not a number) add 1 to counter
+                if (testWhite(String(secondNum))){
+                    // console.log("secondNum is white space");
+
+                    //add counter to count possible games and compare to  how wmany subgames
+                    handCounter = handCounter + 1;
+                    console.log('handCounter after single digit: ', handCounter);
+                } else {
+                    let num = Number(currentSubGame[k].charAt(0)+ secondNum);
+
+                    let colour = currentSubGame[k].slice(2).trimStart();
+                    //if colour contains . remove it
+                    if (colour.charAt(colour.length-1) == "."){
+                        colour = colour.slice(0,colour.length-1);
+
+                    }
+                    compareCubes(colour, num);
+                }
+
+                //at end of loop if counter = currentsubgame.length, add to subGameCounter
+
+                if (handCounter == currentSubGame.length){
+                    subGameCounter = subGameCounter + 1;
+                    console.log('subGameCounter: ', subGameCounter);
+                }
+
+                function testWhite(x){
+                    let white = new RegExp(/^\s$/);
+                    return white.test(currentSubGame[k].charAt(1));
+                };
+
+                function compareCubes(colour, num){
+                    if ((colour == 'red' && num <= 12) || (colour == 'green' && num <= 13) || (colour == 'blue' && num <= 14)){
+                        handCounter = handCounter + 1;
+                            console.log('handCounter after double digit: ', handCounter);
+                    } else {
+                       console.log("game not possible");
+                        return;
+                    }
+                };
+                
+            }
+            if (subGameCounter == subGameArray.length){
+                    arrOfPossibleGames.push(gameID);
+                    console.log('arrOfPossibleGames: ',arrOfPossibleGames);
+            }
+        }
+    }
+
+    //go through array and add all values
+    const sum = arrOfPossibleGames.reduce((partialSum, a) => partialSum + a, 0);
+    console.log('sum: ', sum);
+  
 
 
 
-// para.textContent = gameArray;
+
 
 
 
